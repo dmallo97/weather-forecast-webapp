18cf10c6f0e7cbdfd497ccbbdcf92ed9cd44bd75
dmallo97 <diegom1891@hotmail.com>
2021-05-05 11:21:03 -0300

    Reestructuración de proyecto

---
+++ weather-forecast/src/containers/WeatherApp.js
@@ -0,0 +1,15 @@
+import '../styles/WeatherApp.css'; 
+import React, { useState } from 'react';
+import CurrentWeather from '../components/CurrentWeather';
+import Forecast from '../components/Forecast';
+import styled from 'styled-components';
+
+const WeatherApp = () => {
+
+  return(
+    <>
+    </>
+  );
+}
+
+export default WeatherApp;
